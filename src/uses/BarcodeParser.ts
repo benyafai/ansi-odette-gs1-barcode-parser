42c76e23c7b62f2d5c12fc1b5edef231f0102402
Ben Yafai <ben@yaf.ai>
2025-02-11 17:41:44 +0000

    ✨ More Overrides to process ANSI UoM, Date, and Hazards!

--- src/uses/BarcodeParser.ts
+++ src/uses/BarcodeParser.ts
@@ -1,11 +1,12 @@
 import { type AIList, type AIType } from "../types/ApplicationIdentifierType";
+import { ANSIDateQualifier } from "./ANSI.DateQualifier";
+import { ANSIUnitOfMeasure } from "./ANSI.UnitOfMeasure";
+import { ANSIHazardQualifier } from "./ANSI.HazardQualifier";
 
 export const BarcodeParser = (
   barcode: string,
   ApplicationIdentifiers: AIList
 ) => {
-  // let odettePresent = new RegExp(/^\[\)>\x1e06\x1d.*\x1e\x04$/).test(barcode);
-  // let gs1Present = new RegExp(/^\]d2.*/).test(barcode);
   let barcodeData = barcode
     .replace(/^\[\)>\x1e06\x1d/, "") // Strip Odette Header
     .replace(/\x1e\x04$/, "") // Strip Odette Trailer
@@ -20,18 +21,52 @@ export const BarcodeParser = (
           let reg = regex.exec(bCode);
           if (reg && 2 in reg) {
             let ai = reg[1];
+            let value = reg[2];
             result[ai] = <AIType>{};
             result[ai].identifier = ai;
             result[ai].title = ApplicationIdentifiers[Identifier].title;
             result[ai].desc = ApplicationIdentifiers[Identifier].desc;
             result[ai].format = ApplicationIdentifiers[Identifier].format;
-            result[ai].value = reg[2];
+            result[ai].value = value;
             // Overrides: Do we need to override our value type?
             if (IdentifierType.overrideType == "number") {
-              result[ai].value = parseFloat(reg[2]);
+              result[ai].value = parseFloat(value);
             }
             if (IdentifierType.overrideType == "currency") {
-              result[ai].value = parseFloat(reg[2]).toFixed(2);
+              result[ai].value = parseFloat(value).toFixed(2);
+            }
+            if (IdentifierType.overrideType == "ANSIDateQualifier") {
+              let rawDate = value.slice(0, -3);
+              let timestamp = Date.UTC(
+                parseInt(
+                  rawDate.length == 8
+                    ? rawDate.slice(0, 4)
+                    : new Date().getFullYear().toString().slice(0, 2) +
+                        rawDate.slice(0, 2)
+                ),
+                parseInt(
+                  rawDate.length == 8
+                    ? rawDate.slice(4, 6)
+                    : rawDate.slice(2, 4)
+                ) - 1,
+                parseInt(
+                  rawDate.length == 8
+                    ? rawDate.slice(6, 8)
+                    : rawDate.slice(4, 6)
+                )
+              );
+              // console.log(Date.UTC(89, 0, 25));
+              result[ai].processed =
+                new Date(timestamp).toLocaleDateString() +
+                " " +
+                ANSIDateQualifier[value.slice(-3)];
+            }
+            if (IdentifierType.overrideType == "ANSIHazardQualifier") {
+              result[ai].processed = ANSIHazardQualifier[value.slice(0, 1)];
+            }
+            if (IdentifierType.overrideType == "ANSIUnitOfMeasure") {
+              result[ai].processed =
+                value.slice(0, -2) + " " + ANSIUnitOfMeasure[value.slice(-2)];
             }
           }
         }
